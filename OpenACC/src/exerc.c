940d741fc1f5908ff2f10bef1ab6aa1bfc4ccf05
EliseuPHP <eliseuphp@gmail.com>
2020-11-23 13:14:33 -0300

    Realizada uma documantação do código

--- OpenACC/src/exerc.c
+++ OpenACC/src/exerc.c
@@ -4,9 +4,8 @@
 #include <string.h>
 #include <sys/time.h>
 
-int printMatrix(int rows, int cols, float *a);
-int readMatrix(unsigned int rows, unsigned int cols, float *a, const char *filename);
-int writeMatrix(unsigned int rows, unsigned int cols, float *a, const char *filename);
+int readMatrix(unsigned int rows, unsigned int cols, float *a, const char *filename);  //Função para preencher uma matriz com os dados recebidos de um arquivo.
+int writeMatrix(unsigned int rows, unsigned int cols, float *a, const char *filename); //Função para preencher um arquivo com os dados recebidos de uma matriz.
 
 int y;
 int w;
@@ -14,7 +13,7 @@ int v;
 
 int main(int argc, char *argv[])
 {
-  // pega variaveis do argv
+  // Variáveis para receber valores do argv
   y = atoi(argv[1]);
   w = atoi(argv[2]);
   v = atoi(argv[3]);
@@ -27,45 +26,34 @@ int main(int argc, char *argv[])
   strcpy(arqC, argv[6]);
   strcpy(arqD, argv[7]);
 
-  // cria matrizes
-
+  // Criaçao e alocação das matrizes em uma etapa
   float *matrizA = (float *)malloc(y * w * sizeof(float));
   float *matrizB = (float *)malloc(w * v * sizeof(float));
   float *matrizC = (float *)malloc(v * 1 * sizeof(float));
   float *matrizD = (float *)malloc(y * 1 * sizeof(float));
   float *aux = (float *)malloc(y * v * sizeof(float));
 
-  // pegar dados nos arquivos
-
+  // Ler os valores dos dados arquivos pelo ArgV para suas respectivas matrizes
   readMatrix(y, w, matrizA, arqA);
   readMatrix(w, v, matrizB, arqB);
   readMatrix(v, 1, matrizC, arqC);
 
-  // teste pra ver os dados
-
-  // printf("%s\n\n", arqA);
-  // printMatrix(y, w, matrizA);
-  // printf("%s\n\n", arqB);
-  // printMatrix(w, v, matrizB);
-  // printf("%s\n\n", arqC);
-  // printMatrix(v, 1, matrizC);
-
-  // inicio multiplicacao
-
+  // Declaração de variáveis que serão usadas para a multiplicação
   int i;
   int j;
   int k;
 
   double soma = 0.0;
 
+  // Variáveis para o controle de tempo
   double time_spent = 0.0;
-
-  // Inicio de uma regiao paralela
 
   struct timeval start, end;
 
+  // Inicia a contagem de tempo da região paralela
   gettimeofday(&start, NULL);
 
+  // Inicio de uma regiao paralela
 #pragma acc data copy(matrizA, matrizB, matrizC, aux) copyout(matrizD)
   {
     // A*B = aux
@@ -84,8 +72,8 @@ int main(int argc, char *argv[])
         }
       }
     }
-    // aux*C = D
 
+    // aux*C = D
 #pragma acc parallel
     {
 #pragma acc loop collapse(3)
@@ -96,17 +84,23 @@ int main(int argc, char *argv[])
           // matrizD[i * 1 + j] = 0.0;
           for (k = 0; k < v; k++)
           {
-            // printf("%.2f   =   %.2f   +   %.2f   *   %.2f\n", matrizD[i * 1 + j], matrizD[i * 1 + j], aux[i * v + k], matrizC[k * 1 + j]);
             matrizD[i * 1 + j] = matrizD[i * 1 + j] + aux[i * v + k] * matrizC[k * 1 + j];
           }
         }
       }
     }
   }
-// Fim da regiao paralela
+  // Fim da regiao paralela
 
-// Soma
-#pragma acc parallel loop collapse(2) reduction(+:soma)
+  // Desaloca matrizes já utilizadas
+  free(matrizA);
+  free(matrizB);
+  free(matrizC);
+  free(aux);
+
+  // Redução da matrizD por soma
+#pragma acc parallel loop collapse(2) reduction(+ \
+                                                : soma)
   for (i = 0; i < y; i++)
   {
     for (j = 0; j < 1; j++)
@@ -114,14 +108,24 @@ int main(int argc, char *argv[])
       soma += matrizD[i * 1 + j];
     }
   }
+
+  //  Fim da contagem do tempo
   gettimeofday(&end, NULL);
 
+  // Cálculo do tempo de execução
   long seconds = (end.tv_sec - start.tv_sec);
   long micros = ((seconds * 1000000) + end.tv_usec) - (start.tv_usec);
 
+  // Printa o tempo de execução da regiao paralela
   // printf("Time elpased is %d seconds and %d micros\n", seconds, micros);
 
+  // Escreve matriz D no arquivo
   writeMatrix(y, 1, matrizD, arqD);
+
+  // Desaloca matriz já utilizada
+  free(matrizD);
+
+  // Printa o resultado da redução da matriz D
   printf("%.2f\n", soma);
 }
 
@@ -147,21 +151,6 @@ int readMatrix(unsigned int rows, unsigned int cols, float *a, const char *filen
   }
   // printf("\npassou\n");
   fclose(pf);
-  return 1;
-}
-
-int printMatrix(int rows, int cols, float *a)
-{
-  register unsigned int i, j;
-
-  for (i = 0; i < rows; i++)
-  {
-    for (j = 0; j < cols; j++)
-    {
-      printf("%.2f\t", a[i * cols + j]);
-    }
-    printf("\n");
-  }
   return 1;
 }
 
