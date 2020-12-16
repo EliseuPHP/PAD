105c676dd0ffbbd63581906f9bef4fd9f911d0c1
EliseuPHP <eliseuphp@gmail.com>
2020-12-16 15:08:56 -0300

    TESTE

--- MPI/src/exerc.c
+++ MPI/src/exerc.c
@@ -20,12 +20,9 @@ int v;
 int main(int argc, char *argv[])
 {
     int quantProcs, rank, numWorkers, fonte, dest, mtype, rows, averow, extra, offset, rc;
-    int yAverow, yExtra, yOffset,
-        wAverow, wExtra, wOffset,
-        vAverow, vExtra, vOffset;
-    // double a[NRA][NCA],
-    //     b[NCA][NCB],
-    //     c[NRA][NCB];
+    // int yAverow, yExtra, yOffset,
+    //     wAverow, wExtra, wOffset,
+    //     vAverow, vExtra, vOffset;
     MPI_Status status;
 
     // Variáveis para receber valores do argv
@@ -67,27 +64,13 @@ int main(int argc, char *argv[])
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &quantProcs);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    if (quantProcs < 2)
-    {
-        printf("Need at least two MPI tasks. Quitting...\n");
-        MPI_Abort(MPI_COMM_WORLD, rc);
-        exit(1);
-    }
+   
     numWorkers = quantProcs - 1;
 
     /**************************** master task ************************************/
     if (rank == MASTER)
     {
-        printf("mpi_mm has started with %d tasks.\n", quantProcs);
-        // printf("Initializing arrays...\n");
-        for (i = 0; i < y; i++)
-            for (j = 0; j < w; j++)
-                a[i][j] = i + j;
-        for (i = 0; i < w; i++)
-            for (j = 0; j < v; j++)
-                b[i][j] = i * j;
-
-        /* Measure start time */
+        
         double start = MPI_Wtime();
 
         /* Send matrix data to the worker tasks */
@@ -98,28 +81,24 @@ int main(int argc, char *argv[])
         for (dest = 1; dest <= numWorkers; dest++)
         {
             rows = (dest <= extra) ? averow + 1 : averow;
-            // printf("Sending %d rows to task %d offset=%d\n",rows,dest,offset);
             MPI_Send(&offset, 1, MPI_INT, dest, mtype, MPI_COMM_WORLD);
             MPI_Send(&rows, 1, MPI_INT, dest, mtype, MPI_COMM_WORLD);
-            MPI_Send(&matrizA[posicao(offset, 0, w)], rows * w, MPI_DOUBLE, dest, mtype,
+            MPI_Send(&matrizA[posicao(offset, 0, w)], rows * w, MPI_FLOAT, dest, mtype,
                      MPI_COMM_WORLD);
-            MPI_Send(&matrizB, w * v, MPI_DOUBLE, dest, mtype, MPI_COMM_WORLD);
+            MPI_Send(&matrizB, w * v, MPI_FLOAT, dest, mtype, MPI_COMM_WORLD);
             offset = offset + rows;
         }
 
-        /* Receive results from worker tasks */
         mtype = FROM_WORKER;
         for (i = 1; i <= numWorkers; i++)
         {
             fonte = i;
             MPI_Recv(&offset, 1, MPI_INT, fonte, mtype, MPI_COMM_WORLD, &status);
             MPI_Recv(&rows, 1, MPI_INT, fonte, mtype, MPI_COMM_WORLD, &status);
-            MPI_Recv(&aux[posicao(offset, 0, v)], rows * v, MPI_DOUBLE, fonte, mtype,
+            MPI_Recv(&aux[posicao(offset, 0, v)], rows * v, MPI_FLOAT, fonte, mtype,
                      MPI_COMM_WORLD, &status);
-            // printf("Received results from task %d\n",fonte);
         }
 
-        /* Print results */
 
         printf("******************************************************\n");
         printf("Result Matrix:\n");
@@ -142,8 +121,8 @@ int main(int argc, char *argv[])
         mtype = FROM_MASTER;
         MPI_Recv(&offset, 1, MPI_INT, MASTER, mtype, MPI_COMM_WORLD, &status);
         MPI_Recv(&rows, 1, MPI_INT, MASTER, mtype, MPI_COMM_WORLD, &status);
-        MPI_Recv(&matrizA, rows * w, MPI_DOUBLE, MASTER, mtype, MPI_COMM_WORLD, &status);
-        MPI_Recv(&matrizB, w * v, MPI_DOUBLE, MASTER, mtype, MPI_COMM_WORLD, &status);
+        MPI_Recv(&matrizA, rows * w, MPI_FLOAT, MASTER, mtype, MPI_COMM_WORLD, &status);
+        MPI_Recv(&matrizB, w * v, MPI_FLOAT, MASTER, mtype, MPI_COMM_WORLD, &status);
 
         for (i = 0; i < rows; i++)
         {
@@ -159,7 +138,7 @@ int main(int argc, char *argv[])
         mtype = FROM_WORKER;
         MPI_Send(&offset, 1, MPI_INT, MASTER, mtype, MPI_COMM_WORLD);
         MPI_Send(&rows, 1, MPI_INT, MASTER, mtype, MPI_COMM_WORLD);
-        MPI_Send(&aux, rows * v, MPI_DOUBLE, MASTER, mtype, MPI_COMM_WORLD);
+        MPI_Send(&aux, rows * v, MPI_FLOAT, MASTER, mtype, MPI_COMM_WORLD);
     }
     MPI_Finalize();
 }
