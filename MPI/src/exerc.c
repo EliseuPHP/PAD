2d6b4538405e41eeadc5874bfbe5a3db98fcd60a
EliseuPHP <eliseuphp@gmail.com>
2020-12-16 21:29:12 -0300

    a

--- MPI/src/exerc.c
+++ MPI/src/exerc.c
@@ -192,10 +192,11 @@ int main(int argc, char *argv[])
             for (i = 0; i < auxRows; i++)
             {
                 matrizD[posicao(i, k, 1)] = 5.0;
-                // for (j = 0; j < v; j++)
-                // {
-                //     matrizD[i * 1 + k] = matrizD[i * 1 + k] + aux[i * v + j] * matrizC[j * 1 + k];
-                // }
+                for (j = 0; j < v; j++)
+                {
+                    printf("%.2f", aux[i * v + j]);
+                    matrizD[i * 1 + k] = matrizD[i * 1 + k] + aux[i * v + j] * matrizC[j * 1 + k];
+                }
             }
         }
 
